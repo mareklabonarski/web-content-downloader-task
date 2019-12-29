c02a3d48852609c15728175de6d1c8addb6147f5
Marek Labonarski <mareklabonarski@gmail.com>
2019-12-29 09:36:02 -0500

    Forgot about those 2 files:)

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
+docker-compose down
+docker volume rm semantive_media || true
 docker-compose up -d --build
---
+++ fetch_some_data.sh
@@ -0,0 +1,5 @@
+curl -X POST 'http://127.0.0.1/api/images_tasks/' -H 'Content-Type: application/json' -d '{ "url": "http://www.wp.pl"}'
+curl -X POST 'http://127.0.0.1/api/images_tasks/' -H 'Content-Type: application/json' -d '{ "url": "http://www.onet.pl"}'
+curl -X POST 'http://127.0.0.1/api/text_tasks/' -H 'Content-Type: application/json' -d '{ "url": "http://www.onet.pl"}'
+curl -X POST 'http://127.0.0.1/api/text_tasks/' -H 'Content-Type: application/json' -d '{ "url": "http://www.onet.pl"}'
+
